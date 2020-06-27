3b641a0831747d11aa162cfdb3bc4dc24bafc6bf
Noel Abellera <noel.abellera@gmail.com>
2020-06-26 23:46:56 -0700

    test commit

---
+++ main.js
@@ -0,0 +1,20 @@
+console.log("Hello World")
+
+let msgArray = ['Hello', 'My', 'Name', 'is', 'Ellis']
+
+var showMsg = document.getElementById('msg')
+
+
+document.getElementById("btn")
+.addEventListener('click', changeMe);
+
+
+var changeMe = () => console.log("Hello Yo")
+
+function changeMe() {
+    let resultMsg
+    msgArray .forEach(e => {
+        resultMsg = msgArray[Math.floor(Math.random()*msgArray.length)]
+    })
+    console.log(resultMsg)
+}
