9eee3429deafb957779e147fe26b2d5931487f46
Noel Abellera <noel.abellera@gmail.com>
2020-06-28 19:33:03 -0700

    Add textbox practice

--- main.js
+++ main.js
@@ -3,11 +3,16 @@ console.log("Hello World")
 let msgArray = ['Hello', 'My', 'Name', 'is', 'Ellis']
 
 var showMsg = document.getElementById('msg')
+var name = document.getElementById('nameBox')
+let showName = document.getElementById('name')
 
 
 document.getElementById("btn")
 .addEventListener('click', changeMe);
 
+
+document.getElementById('nameBtn')
+.addEventListener('click', changeName)
 
 var changeMe = () => console.log("Hello Yo")
 
@@ -19,3 +24,15 @@ function changeMe() {
     console.log(resultMsg)
     showMsg.innerHTML = resultMsg
 }
+
+function changeName() {
+    console.log("Hello")
+    if(name = '') {
+        showName.innerHTML = "Please Enter a name"
+    }else {
+        showName.innerHTML = name
+    }
+    showName.innerHTML ="test"
+}
+
+console.log(name)
